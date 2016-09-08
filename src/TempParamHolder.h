318a4e862263349d27fcc0d9ef6904a98ea4a09f
Ondra Musil <ondrej.musil@qoretechnologies.com>
2016-09-08 16:35:09 +0900

    Fixed indentation in TempParamHolder.h

--- src/TempParamHolder.h
+++ src/TempParamHolder.h
@@ -42,40 +42,40 @@ struct BoolWrapper {
 //! Class used by @ref ODBCStatement for temporary storage of SQL parameters.
 class TempParamHolder {
 private:
-   std::vector<char*> strings;
-   std::vector<BoolWrapper> bools;
-   std::vector<SQLLEN> lengths;
-   std::vector<TIMESTAMP_STRUCT> dates;
-   std::vector<SQL_INTERVAL_STRUCT> times;
+    std::vector<char*> strings;
+    std::vector<BoolWrapper> bools;
+    std::vector<SQLLEN> lengths;
+    std::vector<TIMESTAMP_STRUCT> dates;
+    std::vector<SQL_INTERVAL_STRUCT> times;
 
 public:
 
-   TempParamHolder() {
-      strings.reserve(16);
-      bools.reserve(8);
-      lengths.reserve(32);
-      dates.reserve(8);
-      times.reserve(8);
-   }
-   ~TempParamHolder() { clear(); }
+    TempParamHolder() {
+        strings.reserve(16);
+        bools.reserve(8);
+        lengths.reserve(32);
+        dates.reserve(8);
+        times.reserve(8);
+    }
+    ~TempParamHolder() { clear(); }
 
-   char* addC(char* s) { strings.push_back(s); return s; }
-   bool* addB(bool b) { bools.push_back(b); return &(bools[bools.size()-1].val); }
-   SQLLEN* addL(SQLLEN l) { lengths.push_back(l); return &(lengths[lengths.size()-1]); }
-   TIMESTAMP_STRUCT* addD(TIMESTAMP_STRUCT d) { dates.push_back(d); return &(dates[dates.size()-1]); }
-   SQL_INTERVAL_STRUCT* addT(SQL_INTERVAL_STRUCT t) { times.push_back(t); return &(times[times.size()-1]); }
+    char* addC(char* s) { strings.push_back(s); return s; }
+    bool* addB(bool b) { bools.push_back(b); return &(bools[bools.size()-1].val); }
+    SQLLEN* addL(SQLLEN l) { lengths.push_back(l); return &(lengths[lengths.size()-1]); }
+    TIMESTAMP_STRUCT* addD(TIMESTAMP_STRUCT d) { dates.push_back(d); return &(dates[dates.size()-1]); }
+    SQL_INTERVAL_STRUCT* addT(SQL_INTERVAL_STRUCT t) { times.push_back(t); return &(times[times.size()-1]); }
 
-   void clear() {
-      unsigned int count = strings.size();
-      for (unsigned int i = 0; i < count; i++)
-         delete [] (strings[i]);
+    void clear() {
+        unsigned int count = strings.size();
+        for (unsigned int i = 0; i < count; i++)
+            delete [] (strings[i]);
 
-      strings.clear();
-      bools.clear();
-      lengths.clear();
-      dates.clear();
-      times.clear();
-   }
+        strings.clear();
+        bools.clear();
+        lengths.clear();
+        dates.clear();
+        times.clear();
+    }
 };
 
 #endif // _QORE_TEMPPARAMHOLDER_H
