7553086395c95e554dc14143227cf6ab4d7ca6bb
David Nichols <david@qore.org>
2022-05-04 21:30:15 +0200

    reenabled tests

--- test/docker_test/test-alpine.sh
+++ test/docker_test/test-alpine.sh
@@ -47,6 +47,15 @@ chown -R qore:qore ${MODULE_SRC_DIR}
 # run the tests
 export QORE_MODULE_DIR=${MODULE_SRC_DIR}/qlib:${QORE_MODULE_DIR}
 cd ${MODULE_SRC_DIR}
-#for test in test/*.qtest; do
-#    gosu qore:qore qore $test -vv
-#done
+for test in test/*.qtest; do
+    gosu qore:qore qore $test -vv
+    RESULTS="$RESULTS $?"
+done
+
+# check the results
+for R in $RESULTS; do
+    if [ "$R" != "0" ]; then
+        exit 1 # fail
+    fi
+done
+
